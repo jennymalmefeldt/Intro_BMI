dac14a2799a96c07f6426b63d5ab0e4e9a8e95da
jenny_malmefeldt <jennymalmefeldt@gmail.com>
2020-08-12 11:50:48 +0200

    Just Playing around

--- assets/js/bmiCalculator.js
+++ assets/js/bmiCalculator.js
@@ -1,3 +1,25 @@
-const calculateMetricBMI = (weight,height ) => {
-    return 20
+const calculateMetricBMI = (weight, height) => {
+    let result 
+    result = (weight / ((height / 100) * (height / 100))).toFixed(2)
+
+    return result
+}
+
+
+const getBMIClassification = (value) => {
+
+    if (value < 18.5) {
+        return "Underwight"
+    } else if (value < 25) {
+        return "Normal weight"
+    } else if (value < 30) {
+        return "Over weight"
+    } else if (value < 35) {
+        return "Obesity class 1"
+    } else if (value < 40.1) {
+        return "Obesity class 2"
+    } else {
+        return "Extream Obesity class 3"
+    }
+
 }
--- assets/js/bmiUI.js
+++ assets/js/bmiUI.js
@@ -1,9 +1,11 @@
 const calculateBMI = ()=> {
     let weightInput = document.getElementById("weight")
     let heightInput = document.getElementById("height")
-    let results = calculateMetricBMI(weightInput.nodeValue, heightInput.value)
+    let results = calculateMetricBMI(parseInt(weightInput.nodeValue) ,parseInt(heightInput.value))
+    let message = getBMIClassification(parsFloat(results))
     let displayResultsElement = document.getElementById('results')
-    displayResultsElement.innerHTML = `<h1>Your BMI value is: ${results}</h1>`
-    debugger
+    displayResultsElement.innerHTML = `<h1>Your BMI value is: ${results} and you are deemed ${message}</h1>`
+    
+    
 
 }
